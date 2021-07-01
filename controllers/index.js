99da49002d9d35a4699978597d80279c894c276c
ariftoteles <arif.santoso4@gmail.com>
2021-07-01 14:26:08 +0700

    add queue using presistent db sqlite

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,51 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 const axios = require("axios");
 const got = require("got");
-const kue = require("kue");
-const queue = kue.createQueue({
-  redis: {
-    host: "redis",
-    port: 6379,
-  },
-});
+const Queue = require("node-persistent-queue");
+const q = new Queue(__dirname + "/db.sqlite", 20);
 
-// donwload and upload file running on queue
-queue.process("saveFile", async (job, done) => {
+// // =================== QUEUE USING 'KUE REDIS' ==================
+// const kue = require("kue");
+// const queue = kue.createQueue({
+//   // redis: {
+//   //   host: "redis",
+//   //   port: 6379,
+//   // },
+// });
+// // // donwload and upload file running on queue
+// queue.process("saveFile", (job, done) => {
+//   try {
+//     const { fileName, idx } = job.data.items;
+//     const connectionString = process.env.CONNECTION_STRING;
+//     const container = process.env.CONTAINER_NAME;
+//     const secret = process.env.SECRET;
+//     const data = got.stream(
+//       `https://portal.hoiio.net/_o/v1/callRecordings/${fileName}?secret=${secret}`
+//     );
+//     // console.log(data);
+
+//     const blobServiceClient =
+//       BlobServiceClient.fromConnectionString(connectionString);
+
+//     const blobName = `${idx}-${fileName}`;
+//     const containerClient = blobServiceClient.getContainerClient(container);
+//     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+//     const uploadBlobResponse = blockBlobClient.uploadStream(data);
+//     console.log(uploadBlobResponse, new Date());
+//     done(null, "finish");
+//   } catch (error) {
+//     console.log(error);
+//     done(error);
+//   }
+// });
+
+// // ================== QUEUE USING 'PRESISTEN QUEUE' ===================
+q.on("next", (task) => {
+  console.log("Queue contains " + q.getLength() + " job/s");
+  console.log("Process task: ");
+  console.log(task.job.fileName);
   try {
-    const { fileName, idx } = job.data.items;
+    const { fileName, idx } = task.job;
     const connectionString = process.env.CONNECTION_STRING;
     const container = process.env.CONTAINER_NAME;
     const secret = process.env.SECRET;
@@ -20,48 +53,59 @@ queue.process("saveFile", async (job, done) => {
       `https://portal.hoiio.net/_o/v1/callRecordings/${fileName}?secret=${secret}`
     );
 
-    const blobServiceClient = await BlobServiceClient.fromConnectionString(
-      connectionString
-    );
+    const blobServiceClient =
+      BlobServiceClient.fromConnectionString(connectionString);
 
     const blobName = `${idx}-${fileName}`;
-    const containerClient = await blobServiceClient.getContainerClient(
-      container
-    );
-    const blockBlobClient = await containerClient.getBlockBlobClient(blobName);
-    const uploadBlobResponse = await blockBlobClient.uploadStream(data);
-    console.log(blobName, new Date());
-    done(null, "finish");
+    const containerClient = blobServiceClient.getContainerClient(container);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = blockBlobClient.uploadStream(data);
+    console.log("success", new Date());
+    q.done();
   } catch (error) {
     console.log(error);
-    done(error);
+    q.done(error);
   }
+});
+q.open().then(() => {
+  q.start();
 });
 
 class Controller {
   static async saveAudio(req, res) {
     try {
-      const fileName = req.body.filename;
-      const idx = req.body.idx;
+      // // ============= queue using 'kue redis' ===============
+      // const fileName = req.body.filename;
+      // const idx = req.body.idx;
+      // const job = queue.create("saveFile", {
+      //   items: {
+      //     fileName,
+      //     idx,
+      //   },
+      // });
+      // job.on("failed", (err) => {
+      //   throw new Error(err);
+      // });
+      // job.on("complete", (result) => {
+      //   console.log("complete");
+      // });
+      // job.save();
 
-      const job = queue.create("saveFile", {
-        items: {
-          fileName,
-          idx,
-        },
-      });
-      job.on("failed", (err) => {
-        throw new Error(err);
-      });
-      job.on("complete", (result) => {
-        console.log("complete");
-      });
-      job.save();
+      // console.log("success", new Date());
+      // res.status(200).json({
+      //   message: "success add to queue",
+      // });
 
-      console.log("success", new Date());
-      res.status(200).json({
-        message: "success add to queue",
+      // // ===================== queue using 'node-persistent-queue' ===============
+      const task = {
+        fileName: req.body.filename,
+        idx: req.body.idx,
+      };
+      q.open().then(() => {
+        q.add(task);
       });
+      console.log("response", new Date());
+      res.send("success add queue");
     } catch (error) {
       console.log(error);
       res.status(400).json(error);
